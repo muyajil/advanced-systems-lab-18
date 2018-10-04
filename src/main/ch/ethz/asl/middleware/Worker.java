1741529b558f7ad791fb21302dcded533c143e63
ajilm <ajilm@student.ethz.ch>
2018-10-04 23:11:04 +0200

    setupconections in worker, skeleton for worker tests

--- src/main/ch/ethz/asl/middleware/Worker.java
+++ src/main/ch/ethz/asl/middleware/Worker.java
@@ -9,12 +9,12 @@ import java.net.*;
 import java.io.*;
 
 public class Worker implements Runnable {
-    private List<String> memcachedAddresses;
+    private List<SocketChannel> memcachedConnections;
     private boolean readSharded;
     private final int id;
 
     Worker(List<String> memcachedAddresses, boolean readSharded, int id) {
-        this.memcachedAddresses = memcachedAddresses;
+        this.memcachedConnections = setupConnections(memcachedAddresses);
         this.readSharded = readSharded;
         this.id = id;
     }
@@ -23,6 +23,31 @@ public class Worker implements Runnable {
     public void run(){
         System.out.println("Hello from Worker");
     }
+
+    private List<SocketChannel> setupConnections(List<String> memcachedAddresses){
+        List<SocketChannel> memcachedConnections = new ArrayList<SocketChannel>();
+        for (String address : memcachedAddresses){
+            String[] parts = address.split(":");
+            try{
+                SocketChannel memcachedSocket = SocketChannel.open();
+                InetSocketAddress memcachedAddress = new InetSocketAddress(
+                        parts[0],
+                        Integer.parseInt(parts[1]));
+                memcachedSocket.connect(memcachedAddress);
+                memcachedConnections.add(memcachedSocket);
+            } catch (IOException e){
+                printToStdout("Reporting and exception:");
+                e.printStackTrace(System.out);
+            }
+        }
+
+        return memcachedConnections;
+    }
+
+    private void printToStdout(String text){
+        System.out.println("Time: " + System.nanoTime() + " Worker " + Integer.toString(this.id) + ": " + text);
+    }
+
 }
 
 
---
+++ src/test/ch/ethz/asl/middleware/WorkerTest.java
@@ -0,0 +1,13 @@
+package ch.ethz.asl.middleware;
+
+import junit.framework.TestCase;
+
+public class WorkerTest extends TestCase {
+    public void testTrue() {
+        assertTrue(true);
+    }
+
+    public void testSetupConnections(){
+
+    }
+}
