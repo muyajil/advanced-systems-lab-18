42c45184b1711a96d1101e2c80243d619a8e6b56
ajilm <ajilm@student.ethz.ch>
2018-11-14 21:45:25 +0100

    added possibility to run a single test

--- experimentation/asltesting/run_tests.py
+++ experimentation/asltesting/run_tests.py
@@ -6,18 +6,19 @@ from asltesting.test_runner import TestRunner
 
 TESTS_DIR = './tests/'
 
-def get_tests():
-    return os.listdir(TESTS_DIR)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--test_name', type=str)
     parser.add_argument('--local', action='store_true')
     args = parser.parse_args()
 
     test_configs = []
 
-    for test_name in get_tests():
-        test_configs.append(TestConfiguration(TESTS_DIR, test_name))
+    if args.test_name is not None:
+        test_configs = [TestConfiguration(TESTS_DIR, args.test_name)]
+    else:
+        test_configs = list(map(lambda test_name: TestConfiguration(TESTS_DIR, test_name), os.listdir(TESTS_DIR)))
 
     plotter = Plotter()
     runner = TestRunner(args.local)
--- experimentation/asltesting/test_configuration.py
+++ experimentation/asltesting/test_configuration.py
@@ -4,6 +4,8 @@ import json
 class TestConfiguration(object):
 
     base_dir = None
+    log_dir = None
+    plot_dir = None
     config = None
     name = None
 
@@ -11,4 +13,6 @@ class TestConfiguration(object):
 
         self.base_dir = os.path.join(test_dir, name)
         self.config = json.load(open(os.path.join(self.base_dir, 'config.json')))
+        self.plot_dir = os.path.join(self.base_dir, 'plots')
+        self.log_dir = os.path.join(self.base_dir, 'logs')
         self.name = name
