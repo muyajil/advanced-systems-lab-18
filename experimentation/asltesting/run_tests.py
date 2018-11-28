39b8ec2354e333695a041713599d65b2962224de
ajilm <ajilm@student.ethz.ch>
2018-11-28 20:36:47 +0100

    test names as list

--- experimentation/asltesting/run_tests.py
+++ experimentation/asltesting/run_tests.py
@@ -9,7 +9,7 @@ import time
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--test_name', type=str, default=None)
+    parser.add_argument('--test_names', nargs='+', type=str, default=[], dest='test_names')
     parser.add_argument('--local', action='store_true')
     parser.add_argument('--run_id', type=str, default=None)
     parser.add_argument('--exclude ', nargs='+', type=str, default=[], dest='exclude')
@@ -18,10 +18,10 @@ if __name__ == '__main__':
     test_configs = []
     run_id = args.run_id if args.run_id is not None else int(time.time())
 
-    if args.test_name is not None:
-        test_configs = [TestConfiguration(args.test_name, run_id)]
+    if len(args.test_names) > 0:
+        test_configs = map(lambda test_name: TestConfiguration(test_name, run_id), args.test_names)
     else:
-        test_configs = list(map(lambda test_name: TestConfiguration(test_name, run_id), filter(lambda x: x not in args.exclude, sorted(os.listdir(paths.Absolute.TESTS)))))
+        test_configs = map(lambda test_name: TestConfiguration(test_name, run_id), filter(lambda x: x not in args.exclude, sorted(os.listdir(paths.Absolute.TESTS))))
 
     runner = TestRunner(3, args.local)
     # TODO: Save executed test_configs and allow continuation
